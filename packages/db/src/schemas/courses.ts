3a894d2da232dd9d529c160eb001a615e2628b2f
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-11-12 23:07:23 +0200

    chore(db): add timestamp check in courses table

--- packages/db/src/schemas/courses.ts
+++ packages/db/src/schemas/courses.ts
@@ -1,6 +1,7 @@
-import { relations } from 'drizzle-orm'
+import { relations, sql } from 'drizzle-orm'
 import {
 	boolean,
+	check,
 	pgTable,
 	serial,
 	timestamp,
@@ -11,17 +12,26 @@ import { topics } from './topics.js'
 import { forums } from './forums.js'
 import { announcements } from './announcements.js'
 
-export const courses = pgTable('courses', {
-	id: serial().primaryKey(),
-	startedAt: timestamp('started_at', { withTimezone: true }).notNull(),
-	endedAt: timestamp('ended_at', { withTimezone: true }).notNull(),
-	name: varchar({ length: 255 }).unique().notNull(),
-	img: varchar({ length: 255 }).notNull(),
-	areForumsEnabled: boolean('are_forums_enabled').notNull().default(true),
-	areAnnouncementsEnabled: boolean('are_announcements_enabled')
-		.notNull()
-		.default(true),
-})
+export const courses = pgTable(
+	'courses',
+	{
+		id: serial().primaryKey(),
+		startedAt: timestamp('started_at', { withTimezone: true }).notNull(),
+		endedAt: timestamp('ended_at', { withTimezone: true }).notNull(),
+		name: varchar({ length: 255 }).unique().notNull(),
+		img: varchar({ length: 255 }).notNull(),
+		areForumsEnabled: boolean('are_forums_enabled').notNull().default(true),
+		areAnnouncementsEnabled: boolean('are_announcements_enabled')
+			.notNull()
+			.default(true),
+	},
+	(table) => ({
+		checkConstrait: check(
+			'course-timestamp-check',
+			sql`${table.endedAt} > ${table.startedAt}`,
+		),
+	}),
+)
 
 export const coursesRelations = relations(courses, ({ many }) => ({
 	groups: many(courseGroups),
