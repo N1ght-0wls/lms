dcc4322859b9d499424b349e4a9fe0af6862e26a
Kyrylo Savieliev <kirillofficial0507@gmail.com>
2024-12-11 01:51:35 +0200

    feat(server): add and integrate result container (#10)

--- apps/server/src/modules/users/interfaces/index.ts
+++ apps/server/src/modules/users/interfaces/index.ts
@@ -1,10 +1,11 @@
+import { Result } from '@/core/utils/result.js'
 import { User } from '@/db/types.js'
 import { CREATE_USER_SCHEMA_TYPE } from '@awesome-lms/shared'
 
 interface IUsersRepository {
-	findOne: (id: number) => Promise<User | null>
-	findOneByEmail: (email: string) => Promise<User | null>
-	createOne: (data: CREATE_USER_SCHEMA_TYPE) => Promise<User | null>
+	findOne: (id: number) => Promise<Result<User, null>>
+	findOneByEmail: (email: string) => Promise<Result<User, null>>
+	createOne: (data: CREATE_USER_SCHEMA_TYPE) => Promise<Result<User, null>>
 }
 
 interface UsersModuleDependencies {
--- apps/server/src/modules/users/repositories/UsersRepository.ts
+++ apps/server/src/modules/users/repositories/UsersRepository.ts
@@ -5,6 +5,7 @@ import { CREATE_USER_SCHEMA_TYPE } from '@awesome-lms/shared'
 import { SQL, eq } from 'drizzle-orm'
 import { IUsersRepository } from '../interfaces/index.js'
 import { UsersInjectableDependencies } from '../types/index.js'
+import { Failure, Result, Success } from '@/core/utils/result.js'
 
 export class UsersRepository implements IUsersRepository {
 	private readonly db: DatabaseClient
@@ -13,36 +14,41 @@ export class UsersRepository implements IUsersRepository {
 		this.db = db.client
 	}
 
-	async findOne(id: number): Promise<User | null> {
+	async findOne(id: number): Promise<Result<User, null>> {
 		return this.findOneBy(eq(users.id, id))
 	}
 
-	async findOneByEmail(email: string): Promise<User | null> {
+	async findOneByEmail(email: string): Promise<Result<User, null>> {
 		return this.findOneBy(eq(users.email, email))
 	}
 
-	async createOne(data: CREATE_USER_SCHEMA_TYPE): Promise<User | null> {
-		const hasFirst = await this.db.select().from(users).limit(1)
+	async createOne(data: CREATE_USER_SCHEMA_TYPE): Promise<Result<User, null>> {
+		try {
+			const hasFirst = await this.db.select().from(users).limit(1)
 
-		const result = await this.db
-			.insert(users)
-			.values({ ...data, img: '/', role: hasFirst ? 'user' : 'admin' })
-			.returning()
+			const result = await this.db
+				.insert(users)
+				.values({ ...data, img: '/', role: hasFirst ? 'user' : 'admin' })
+				.returning()
 
-		const user = result.at(0)
+			const user = result.at(0) as User
 
-		if (!user) return null
-
-		return user
+			return Success(user)
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+		} catch (e) {
+			return Failure(null)
+		}
 	}
 
-	private async findOneBy(condition: SQL<unknown>): Promise<User | null> {
+	private async findOneBy(
+		condition: SQL<unknown>,
+	): Promise<Result<User, null>> {
 		const result = await this.db.select().from(users).where(condition)
 
 		const user = result.at(0)
 
-		if (!user) return null
+		if (!user) return Failure(null)
 
-		return user
+		return Success(user)
 	}
 }
